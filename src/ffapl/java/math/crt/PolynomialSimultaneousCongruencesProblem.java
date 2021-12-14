41c76f14624e9d9bec5f0ba94d1e1fe890f1fa49
Dominic Weinberger <doweinberger@edu.aau.at>
2021-12-14 22:48:00 +0100

    updates PolynomialSimultaneousCongruencesProblem to possibly solve problems with non-coprime moduli. Updates accompanying PolynomialSimultaneousCongruences.ffapl as well.

--- src/ffapl/java/math/crt/PolynomialSimultaneousCongruencesProblem.java
+++ src/ffapl/java/math/crt/PolynomialSimultaneousCongruencesProblem.java
@@ -1,14 +1,18 @@
 package ffapl.java.math.crt;
 
-import ffapl.java.classes.Polynomial;
 import ffapl.java.classes.PolynomialRC;
 import ffapl.java.exception.FFaplAlgebraicException;
 import ffapl.java.interfaces.IAlgebraicError;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import static ffapl.java.math.Algorithm.*;
 import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
 
 /**
  * Represents a problem were simultaneous congruences need to be solved, which is done via the Chinese Remainder Theorem
@@ -69,17 +73,43 @@ public class PolynomialSimultaneousCongruencesProblem {
 
     /**
      * Solving simultaneous congruences with given congruences and moduli. Calculates the value which solves those congruences
-     * using the Chinese remainder theorem or throws an exception if no such value exists.
-     * Uses different solution strategies based on whether the moduli are pairwise coprime or not.
+     * using the Chinese remainder theorem or throws an exception if no solution exists.
+     * If there are pairs of non-coprime moduli, those are tried to be solved directly. As soon as all moduli are coprime,
+     * the CRT is applied directly.
      * @throws FFaplAlgebraicException
-     *          <CRT_POLYNOMIAL_NONCOPRIME_NOTIMPLEMENTED> currently for non-coprime moduli, since this is case is not yet implemented for polynomials.
      */
-    public Polynomial solve() throws FFaplAlgebraicException {
-        if (valuesArePairwiseCoprime(moduli)) {
-            return solveForCoprimeModuli();
-        } else {
-            throw new FFaplAlgebraicException(null, IAlgebraicError.CRT_POLYNOMIAL_NONCOPRIME_NOTIMPLEMENTED);
+    public PolynomialRC solve() throws FFaplAlgebraicException {
+        List<PolynomialRC> currentCongruences = new ArrayList<>(Arrays.asList(congruences));
+        List<PolynomialRC> currentModuli = new ArrayList<>(Arrays.asList(moduli));
+
+        int[] currentNonCoprimeModuliIndices;
+
+        // As long as congruence-moduli pairs with non-coprime moduli are found ...
+        while ((currentNonCoprimeModuliIndices = findTwoNonCoprimeModuli(currentModuli)) != null) {
+            int firstModuleIndex = currentNonCoprimeModuliIndices[0];
+            int secondModuleIndex = currentNonCoprimeModuliIndices[1];
+
+            // TODO - DOMINIC - 14.12.2021: Works, but could use some refactoring
+            PolynomialRC firstCongruence = currentCongruences.get(firstModuleIndex);
+            PolynomialRC secondCongruence = currentCongruences.get(secondModuleIndex);
+            PolynomialRC firstModule = currentModuli.get(firstModuleIndex);
+            PolynomialRC secondModule = currentModuli.get(secondModuleIndex);
+
+            // ... try to solve them directly, and ...
+            PolynomialRC[] directCongruenceResult = solveSimultaneousCongruenceDirectlyFor(
+                    firstCongruence, firstModule, secondCongruence, secondModule);
+
+            // ... replace the original congruence equation with the solution acquired in the previous step.
+            currentCongruences.remove(firstCongruence);
+            currentCongruences.remove(secondCongruence);
+            currentModuli.remove(firstModule);
+            currentModuli.remove(secondModule);
+
+            currentCongruences.add(directCongruenceResult[0]);
+            currentModuli.add(directCongruenceResult[1]);
         }
+
+        return solveForCoprimeModuli(currentCongruences, currentModuli);
     }
 
     /**
@@ -87,16 +117,16 @@ public class PolynomialSimultaneousCongruencesProblem {
      * @throws FFaplAlgebraicException
      *          <INTERRUPT> if thread is interrupted
      */
-    private PolynomialRC solveForCoprimeModuli() throws FFaplAlgebraicException {
+    private PolynomialRC solveForCoprimeModuli(List<PolynomialRC> congruences, List<PolynomialRC> moduli) throws FFaplAlgebraicException {
         PolynomialRC moduliProduct = productSum(moduli);
-        PolynomialRC result = new PolynomialRC(BigInteger.ZERO, BigInteger.ZERO, characteristic, thread);
+        PolynomialRC result = new PolynomialRC(ZERO, ZERO, characteristic, thread);
         PolynomialRC n, tmp;
-        for (int i = 0; i < moduli.length; i++) {
+        for (int i = 0; i < moduli.size(); i++) {
             isRunning(thread);
-            n = PolynomialRC.divide(moduliProduct, moduli[i])[0];
-            tmp = eea(n, moduli[i])[1];
+            n = PolynomialRC.divide(moduliProduct, moduli.get(i))[0];
+            tmp = eea(n, moduli.get(i))[1];
             tmp.multiply(n);
-            tmp.multiply(congruences[i]);
+            tmp.multiply(congruences.get(i));
             tmp.mod(moduliProduct);
             result.add(tmp);
         }
@@ -108,8 +138,8 @@ public class PolynomialSimultaneousCongruencesProblem {
      * @throws FFaplAlgebraicException
      * 			<INTERRUPT> if thread is interrupted
      */
-    private PolynomialRC productSum(PolynomialRC[] values) throws FFaplAlgebraicException {
-        PolynomialRC result = new PolynomialRC(ONE, ONE, characteristic, thread);
+    private PolynomialRC productSum(Collection<PolynomialRC> values) throws FFaplAlgebraicException {
+        PolynomialRC result = new PolynomialRC(ONE, ZERO, characteristic, thread);
         for (PolynomialRC value : values) {
             isRunning(thread);
             result.multiply(value);
@@ -118,24 +148,68 @@ public class PolynomialSimultaneousCongruencesProblem {
     }
 
     /**
-     * Returns "true" if values are coprime or array is empty, "false" otherwise
-     * @throws FFaplAlgebraicException
-     * 			<INTERRUPT> if thread is interrupted
+     * This function returns the indices of the first to moduli of the given array which are not coprime, if there are any.
+     * If there are non or it's impossible for them to exist due to the array being smaller than 2, null is returned.
      */
-    public boolean valuesArePairwiseCoprime(PolynomialRC[] values) throws FFaplAlgebraicException {
-        if(values.length == 0) {
-            return true;
+    private int[] findTwoNonCoprimeModuli(List<PolynomialRC> moduli) throws FFaplAlgebraicException {
+        if(moduli.size() < 2) {
+            return null;
         }
 
-        Thread thread = values[0].getThread();
-        for (int i = 0; i < values.length; i++) {
-            for (int j = i+1; j < values.length; j++) {
+        for (int i = 0; i < moduli.size(); i++) {
+            for (int j = i+1; j < moduli.size(); j++) {
                 isRunning(thread);
-                if (!gcd(values[i], values[j]).isOne()) {
-                    return false;
+                if (!gcd(moduli.get(i), moduli.get(j)).isOne()) {
+                    return new int[] {i,j};
                 }
             }
         }
-        return true;
+
+        return null;
+    }
+
+    /** If a = b mod d with d = gcd(m1, m2), then x = a mod m1 and x = b mod m2 is equivalent to
+     *  x = a - y * m1 * (a-b)/d mod (m1*m2)/d with d = y * m1 + z * m2 given by the extended Euclidian algorithm
+     *  In this case, this function returns a two-dimensional array containing the new congruence to be fulfilled by x
+     *  and the respective modulus for this congruence. This congruence-module pair can be reduced to replace the two
+     *  pairs given as input to this function.
+     */
+    private PolynomialRC[] solveSimultaneousCongruenceDirectlyFor(PolynomialRC a, PolynomialRC m1, PolynomialRC b, PolynomialRC m2) throws FFaplAlgebraicException {
+        PolynomialRC[] eeaResult = eea(m1, m2);
+        PolynomialRC d = eeaResult[0];
+        PolynomialRC y = eeaResult[1];
+
+        // first, check whether a solution exists;
+        PolynomialRC a_mod_d = (PolynomialRC) a.clone();
+        a_mod_d.mod(d);
+        PolynomialRC b_mod_d = (PolynomialRC) b.clone();
+        b_mod_d.mod(d);
+
+        if (!a_mod_d.equals( b_mod_d)) {
+            throw new FFaplAlgebraicException(null, IAlgebraicError.CRT_NO_SOLUTION);
+        }
+
+        // temp2 := (a - b) / d
+        PolynomialRC temp2 = (PolynomialRC) a.clone();
+        temp2.subtract(b);
+        temp2.divide(d);  // Might throw an exception if there is no multiplicative inverse of d in the ring of the given characteristic
+
+        // temp1 := y * m1 * temp2
+        PolynomialRC temp1 = (PolynomialRC) y.clone();
+        temp1.multiply(m1);
+        temp1.multiply(temp2);
+
+        // Calculate newModulus := (m1*m2)/d
+        PolynomialRC newModulus = (PolynomialRC) m1.clone();
+        newModulus.multiply(m2);
+        newModulus.divide(d);   // Might throw an exception if there is no multiplicative inverse of d in the ring of the given characteristic
+
+        // solution := a - temp1 (mod newModulus)
+        PolynomialRC solution = (PolynomialRC) a.clone();
+        solution.subtract(temp1);
+        solution.mod(newModulus);
+
+        // return {a - y * m1 * (a-b)/d mod newModulus; newModulus}
+        return new PolynomialRC[] {solution, newModulus};
     }
 }
