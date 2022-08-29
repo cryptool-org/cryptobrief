f52422e649e306f7a82d9e8d758f7c348d779a6e
Dominic Weinberger <doweinberger@edu.aau.at>
2022-08-29 17:38:53 +0200

    Isomorphism calculation - Root-finding strategies
    - Adds interface RootFindingStrategy, which provides the method findRootOf which should return a root of the given polynomial in the given finite field.
    - Adds RandomRootFindingStrategy, which implements RootFindingStrategy. It generates random elements of the field and checks whether the generated element is a root of the polynomial until either a root has been found (and is thus returned) or all elements of the field have been checked for being a root (in which case, null is returned).
    - Adds RabinRootFindingStrategy, which implements RootFindingStrategy. This is an implementation of Rabin's root-finding algorithm.
    - Adds NewRabinRootFindingStrategy, which features some performance improvements compared to RabinRootFindingStrategy. For those improvements, some already existing classes needed to be reimplemented. Therefore, the new classes MyGaloisField, GaloisFieldElement and NewPolynomialGF are also added.
    - Adds LinearFactorIsomorphismCalculation, which implements IsomorphismCalculation and has as member an instance of RootFindingStrategy. It calculates an isomorphism between two isomorphic finite fields by first searching for a root of the source fields' irreducible polynomial in the target field. For this, RootFindingStrategy is used.

---
+++ src/ffapl/java/math/isomorphism/calculation/LinearFactorIsomorphismCalculation.java
@@ -0,0 +1,80 @@
+package ffapl.java.math.isomorphism.calculation;
+
+import ffapl.exception.FFaplException;
+import ffapl.java.classes.*;
+import ffapl.java.exception.FFaplAlgebraicException;
+import ffapl.java.interfaces.IAlgebraicError;
+import ffapl.java.math.isomorphism.Isomorphism;
+import ffapl.java.math.isomorphism.calculation.linearfactor.RootFindingStrategy;
+
+import java.math.BigInteger;
+
+/**
+ * This class calculates an isomorphism between two isomorphic finite fields by first searching for a root
+ * of the source fields' irreducible polynomial in the target field. This root is then used to
+ * calculate an isomorphism (see Bach, Eric, et al. Algorithmic number theory: Efficient algorithms. Vol. 1. MIT press, 1996 on page 173)
+ * @author Dominic Weinberger
+ * @version 1.0
+ *
+ */
+public class LinearFactorIsomorphismCalculation implements IsomorphismCalculation {
+
+    private final RootFindingStrategy _rootFindingStrategy;
+
+    public LinearFactorIsomorphismCalculation(RootFindingStrategy rootFindingStrategy) {
+        this._rootFindingStrategy = rootFindingStrategy;
+    }
+
+    /**
+     * Calculates isomorphism using a root of the source fields' irreducible polynomial in the
+     * target field which is found by a root finding strategy passed to the constructor.
+     * @param sourceField source field
+     * @param targetField target field
+     * @return Isomorphism
+     * @throws FFaplAlgebraicException if underlying operations fail
+     */
+    @Override
+    public Isomorphism calculate(GaloisField sourceField, GaloisField targetField) throws FFaplAlgebraicException {
+        return new Isomorphism(
+                sourceField,
+                targetField,
+                calculatePhiFromRoot(
+                        _rootFindingStrategy.findRootOf(sourceField.irrPolynomial(), targetField), targetField),
+                targetField.getThread());
+    }
+
+    /**
+     * Calculates isomorphism transformation matrix "phi" from field A to field B given "alpha" which is a root
+     * of the source field A's irreducible polynomial in the target field B.
+     * I.e. irred_ply_A(alpha) = 0 mod irred_ply_B.
+     * "Phi" is a square m x m - matrix with m = deg(irred_ply_B).
+     * Also Phi = [phi_1  phi_2 ... phi_m] = [alpha^0  alpha^1 ... alpha^(m-1)] all terms mod irred_ply_B
+     * @param alpha the alpha in (X - alpha), where (X - alpha) is a linear factor of the source fields' irreducible polynomial in the target field.
+     * @param targetField target field
+     * @return Matrix<BInteger> phi
+     * @throws FFaplAlgebraicException if underlying operations fail
+     */
+    private Matrix<BInteger> calculatePhiFromRoot(Polynomial alpha, GaloisField targetField) throws FFaplAlgebraicException {
+        int matrixSize = targetField.irrPolynomial().degree().intValue();
+        BInteger[][] phi = new BInteger[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            Polynomial currentResult = alpha.powR(BigInteger.valueOf(i));
+            PolynomialRC currentResultRC = new PolynomialRC(
+                    currentResult.polynomial(),
+                    targetField.characteristic(),
+                    targetField.getThread());
+            currentResultRC.mod(targetField.irrPolynomial());
+
+            for (int j = 0; j < matrixSize; j++) {
+                phi[j][i] = (BInteger) currentResultRC.coefficientAt(BigInteger.valueOf(j));
+            }
+        }
+
+        try {
+            return new Matrix<>(phi, BInteger.valueOf(0, targetField.getThread()));
+        } catch (FFaplException e) {
+            throw new FFaplAlgebraicException(new Object[]{"isomorphism calculation"}, IAlgebraicError.INTERNAL);
+        }
+    }
+}
---
+++ src/ffapl/java/math/isomorphism/calculation/linearfactor/RandomRootFindingStrategy.java
@@ -0,0 +1,58 @@
+package ffapl.java.math.isomorphism.calculation.linearfactor;
+
+import ffapl.java.classes.BInteger;
+import ffapl.java.classes.GaloisField;
+import ffapl.java.classes.PolynomialRC;
+import ffapl.java.exception.FFaplAlgebraicException;
+import ffapl.java.math.Algorithm;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Searching for a root by randomly trying out different polynomials.
+ * @author Dominic Weinberger
+ * @version 1.0
+ *
+ */
+public class RandomRootFindingStrategy implements RootFindingStrategy {
+
+    /**
+     * Creates random polynomials "alpha" with deg(alpha) < deg(f_q) and char(alpha) = char(f_q) until one
+     * "alpha" is found such that f(alpha) = 0 in f_q.
+     * Polynomials which did not work before are stored in a set. In every iteration, polynomials are created randomly
+     * until one is found which is not yet in the set. If the amount of elements in the set is the order of the field,
+     * the algorithm terminates, and returns null, since no root has been found and every element of the field has
+     * been tested.
+     * @param f which is the polynomial for which a root of f in the field f_q is to be found
+     * @param f_q which is the field containing the root a
+     * @return alpha, such that f(alpha) = 0 in f_q
+     * @throws FFaplAlgebraicException if underlying operations fail
+     */
+    @Override
+    public PolynomialRC findRootOf(PolynomialRC f, GaloisField f_q) throws FFaplAlgebraicException {
+        Set<PolynomialRC> randomElementsCache = new HashSet<>();
+        while (BigInteger.valueOf(randomElementsCache.size()).compareTo(f_q.order()) < 0 ) {
+            PolynomialRC alpha;
+
+            do {
+                alpha = Algorithm.getRandomPolynomial(
+                        new BInteger(f_q.irrPolynomial().degree().subtract(BigInteger.ONE), f_q.getThread()),
+                        new BInteger(f_q.characteristic(), f_q.getThread()),
+                        false);
+            } while (randomElementsCache.contains(alpha));
+
+            PolynomialRC currentResult = f.calculate(alpha);
+            currentResult.mod(f_q.irrPolynomial());
+
+            if (currentResult.isZero()) {
+                return alpha;
+            }
+            randomElementsCache.add(alpha);
+        }
+        return null;
+    }
+}
+
+
